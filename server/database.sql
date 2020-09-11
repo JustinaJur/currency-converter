5796891eba2a9c9cabc5dc5f4acb14a38e996f79
JustinaJur <jurgeleviciute.justina@gmail.com>
2020-09-11 08:28:59 +0300

    add logs table

--- server/database.sql
+++ server/database.sql
@@ -6,7 +6,29 @@ CREATE TABLE currency(
   currency_rate VARCHAR(15) NOT NULL
 ) 
 
-CREATE TABLE currency_country(
-  currency_name VARCHAR(50) REFERENCES currency(currency_name),
-  country_name VARCHAR(200) NOT NULL
-) 
+ CREATE FOREIGN TABLE logs
+(
+  log_time timestamp(3) with time zone,
+  user_name text,
+  database_name text,
+  process_id integer,
+  connection_from text,
+  session_id text,
+  session_line_num bigint,
+  command_tag text,
+  session_start_time timestamp with time zone,
+  virtual_transaction_id text,
+  transaction_id bigint,
+  error_severity text,
+  sql_state_code text,
+  message text,
+  detail text,
+  hint text,
+  internal_query text,
+  internal_query_pos integer,
+  context text,
+  query text,
+  query_pos integer,
+  location text,
+  application_name text)
+ SERVER logserver OPTIONS (filename 'log\postgresql.log');
